f4afbef9805d025f516820af7ee9c49198bb3cb9
phuc-tl <62282709+phuc-tulgu@users.noreply.github.com>
2021-11-13 22:03:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ В массиве из 10 целых чисел найти наименьший элемент и поменять его местами с последним элементом: 
+*/
+
+using System;
+
+namespace Lab_6___CSharp
+{
+    class Program
+    {
+        public static void in_put(int [,] arr, int n)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(" Input value of element(s) arr[{0}][{1}]: ",i+1 , j+1);
+                    arr[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public static void out_put(int[,] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(arr[i,j] + "\t" );
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int [,] arr = new int [20,20];
+            in_put(arr, n);
+            out_put(arr, n);
+        }
+    }
+}
